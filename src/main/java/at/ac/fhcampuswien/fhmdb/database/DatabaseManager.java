9575634a29fc4700f0d6e9b2e8f1f756e31109b2
Len4ik62russ <143323691+Len4ik62russ@users.noreply.github.com>
2024-05-01 15:08:56 +0200

    Database Connection added

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/DatabaseManager.java
@@ -0,0 +1,52 @@
+package at.ac.fhcampuswien.fhmdb.database;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static String DB_Url = "jdbc:h2:file: ./src/main/resources/movies";
+    private static String username = "username";
+    private static String password = "password";
+    private static ConnectionSource connectionSource;
+    Dao<MovieEntity, Integer> movieDao;
+    private static DatabaseManager instance;
+
+    private DatabaseManager() {
+        try {
+            createConnectionSource();
+            movieDao = DaoManager.createDao(connectionSource, MovieEntity.class);
+            createTables();
+
+
+        } catch (SQLException e) {
+            System.out.println("Error creating connection source: " + e.getMessage());
+        }
+    }
+
+    public static DatabaseManager getDatabaseManager() {
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+        return instance;
+    }
+
+    private static void createTables() throws SQLException {
+        TableUtils.createTableIfNotExists(connectionSource, MovieEntity.class);
+        TableUtils.createTableIfNotExists(connectionSource, WatchlistMovieEntity.class);
+    }
+
+    public void createConnectionSource() throws SQLException {
+        connectionSource = new JdbcConnectionSource(DB_Url, username, password);
+    }
+
+    public void testDB() throws SQLException {
+        MovieEntity movie = new MovieEntity("Title1", "Description1");
+        movieDao.create(movie);
+    }
+
+}
